3286e52d25ea6c52edd883340f1cb0377973781b
Bruno Schramm Vendruscolo <brnvendruscolo@gmail.com>
2025-11-06 17:28:24 -0300

    add reset_board

--- riscv1.asm
+++ riscv1.asm
@@ -7,7 +7,10 @@ mancala:   .asciz "\n P2 Mancala: "
 p1_mancala: .asciz "\n P1 Mancala: "
 newline:  .asciz "\n"
 barra: .asciz " | "
-msg_play .asciz "Insira o número da casa para a jogada: "
+msg_play: .asciz "Insira o número da casa para a jogada: "
+
+n0: .word 0
+n4: .word 4
 
 .align 2
 
@@ -16,20 +19,45 @@ BOARD_P2: .space 28
 
 .text
 .globl main
-# s0 - turno; s1 - vitórias;
+###################################### s0 - turno; s1 - vitórias, s2 - board p1 s3 - board p2;###########################################v
 
 main:
 call board_init
-li a7,10
-ecall
-board_init:
-la a2, BOARD_P1
-la a3, BOARD_P2
+#li a7,10
+#ecall
+call imprime_tabuleiro
 
-imprime_tabuleiro_p1:
+board_init:
+la s2, BOARD_P1
+la s3, BOARD_P2
+
+
+reset_board: #reseta todas as posicoes do tabuleiro
+li t0, 0
+li t1, 4
+
+sw t0, (s2) 
+sw t1, 4(s2)
+sw t1, 8(s2)
+sw t1, 12(s2)
+sw t1, 16(s2)
+sw t1, 20(s2)
+sw t1, 24(s2)
+
+sw t1, (s3) 
+sw t1, 4(s3)
+sw t1, 8(s3)
+sw t1, 12(s3)
+sw t1, 16(s3)
+sw t1, 20(s3)
+sw t0, 24(s3)
+j ret
+
+imprime_tabuleiro:
+mv a2, s2
+mv a3, s3
 li t0, 0 #indice
 li t1, 7
-
 imprime_p1:
 beq t0, t1, imprime_tabuleiro_p2
 lw t2, 0(a2)
@@ -88,3 +116,5 @@ li a7, 5
 ecall
 mv t1, a0
 
+ret:
+ret
